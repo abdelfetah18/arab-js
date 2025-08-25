bb0ac1de5294f12b1f33d1743e35c407bf909b84
abdelfetah18 <nstart0319@gmail.com>
2025-08-25 13:27:37 +0100

    Add StringLiteral Tests

--- internal/compiler/printer/printer_test.go
+++ internal/compiler/printer/printer_test.go
@@ -66,3 +66,50 @@ func TestVariableDeclaration(t *testing.T) {
 		printer.Write(program)
 	})
 }
+
+func TestStringLiteral(t *testing.T) {
+	t.Run("should parse a double-quoted StringLiteral", func(t *testing.T) {
+		input := "متغير نص = \"أهلا\"؛"
+		parser := compiler.NewParser(compiler.NewLexer(input), false)
+		program := parser.Parse()
+
+		printer := NewPrinter()
+		printer.Write(program)
+
+		expected := "let نص = \"أهلا\";"
+
+		if printer.Writer.Output != expected {
+			t.Errorf("Expected %s, got %s\n", expected, printer.Writer.Output)
+		}
+	})
+
+	t.Run("should parse a single-quoted StringLiteral", func(t *testing.T) {
+		input := "متغير نص = 'أهلا'؛"
+		parser := compiler.NewParser(compiler.NewLexer(input), false)
+		program := parser.Parse()
+
+		printer := NewPrinter()
+		printer.Write(program)
+
+		expected := "let نص = \"أهلا\";"
+
+		if printer.Writer.Output != expected {
+			t.Errorf("Expected %s, got %s\n", expected, printer.Writer.Output)
+		}
+	})
+
+	t.Run("should parse a StringLiteral inside a BlockStatement", func(t *testing.T) {
+		input := "{ اطبع(\"مرحبا\")؛ }"
+		parser := compiler.NewParser(compiler.NewLexer(input), false)
+		program := parser.Parse()
+
+		printer := NewPrinter()
+		printer.Write(program)
+
+		expected := "{\n  اطبع(\"مرحبا\");\n}"
+
+		if printer.Writer.Output != expected {
+			t.Errorf("Expected:\n%s\nGot:\n%s\n", expected, printer.Writer.Output)
+		}
+	})
+}
