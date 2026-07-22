e32648de48aca59c6fa7b0107f883a5ded7eafc7
abdelfetah18 <nstart0319@gmail.com>
2026-07-22 22:43:42 +0100

    Translate error messages to Arabic

--- internal/binder/errors_format.go
+++ internal/binder/errors_format.go
@@ -1,6 +1,6 @@
 package binder
 
 const (
-	CANNOT_REDECLARE_BLOCK_SCOPED_VARIABLE_0 = "Cannot redeclare block-scoped variable '%s'"
-	DUPLICATE_IDENTIFIER_0                   = "Duplicate identifier '%s'"
+	CANNOT_REDECLARE_BLOCK_SCOPED_VARIABLE_0 = "لا يمكن إعادة تعريف متغير في نفس النطاق '%s'"
+	DUPLICATE_IDENTIFIER_0                   = "معرف مكرر '%s'"
 )
--- internal/checker/errors_format.go
+++ internal/checker/errors_format.go
@@ -1,10 +1,10 @@
 package checker
 
 const (
-	TYPE_0_IS_NOT_ASSIGNABLE_TO_TYPE_1                            = "Type %s is not assignable to type %s"
-	CANNOT_FIND_NAME_0                                            = "Cannot find name '%s'"
-	THIS_EXPRESSION_IS_NOT_CALLABLE_TYPE_0_HAS_NO_CALL_SIGNATURES = "This expression is not callable. Type '%s' has no call signatures."
-	EXPECTED_0_ARGUMENTS_BUT_GOT_1                                = "Expected %d arguments, but got %d"
-	ARGUMENT_OF_TYPE_0_IS_NOT_ASSIGNABLE_TO_PARAMETER_OF_TYPE_1   = "Argument of type '%s' is not assignable to parameter of type '%s'"
-	PROPERTY_0_DOES_NOT_EXIST_ON_TYPE_1                           = "Property '%s' does not exist on type '%s'"
+	TYPE_0_IS_NOT_ASSIGNABLE_TO_TYPE_1                            = "لا يمكن إسناد النوع %s إلى النوع %s"
+	CANNOT_FIND_NAME_0                                            = "لا يمكن العثور على المعرف '%s'"
+	THIS_EXPRESSION_IS_NOT_CALLABLE_TYPE_0_HAS_NO_CALL_SIGNATURES = "هذه جملة غير قابلة للاستدعاء. نوع '%s' لا يملك أي تعاريف استدعاء."
+	EXPECTED_0_ARGUMENTS_BUT_GOT_1                                = "المتوقع %d قيم ممررة, لكن حصلت على %d"
+	ARGUMENT_OF_TYPE_0_IS_NOT_ASSIGNABLE_TO_PARAMETER_OF_TYPE_1   = "قيمة ممررة من نوع '%s' ليست قابلة للاسناد إلى متغير دالة من نوع '%s'"
+	PROPERTY_0_DOES_NOT_EXIST_ON_TYPE_1                           = "خاصية '%s' غير موجودة في نوع '%s'"
 )
